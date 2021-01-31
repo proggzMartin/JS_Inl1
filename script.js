cc96bce7752679e9aa337c8b57f23f47b5b17f58
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-01-31 13:45:27 +0100

    Added function fpor adding 'blugs'

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ Vad har jag gjort för att felsöka/undvika buggar?
 //koden som definieras i filen hålls här. Se översta svaret:
 //https://stackoverflow.com/questions/1841916/how-to-avoid-global-variables-in-javascript
 
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing '+
+                    'elit. Pellentesque id sodales eros. Sed quis nulla '+
+                    'pretium, placerat metus eget, condimentum eros. '+
+                    'Ut eu maximus neque. Pellentesque habitant morbi '+
+                    'tristique senectus et netus et malesuada fames ac '+
+                    'turpis egestas. Donec tincidunt gravida laoreet. '+
+                    'In molestie maximus eleifend. Duis tempus erat nec '+
+                    'mauris pretium, et ultrices odio pulvinar. Nulla '+
+                    'feugiat elit nisi, pellentesque facilisis est '+
+                    'tincidunt ac. Nam interdum auctor lacus, sit amet '+
+                    'dignissim justo. Vivamus consectetur egestas maximus.';
 (function() { 
   "use strict";
 
@@ -33,8 +44,11 @@ Vad har jag gjort för att felsöka/undvika buggar?
                 "icke-initialiserad variabler. Felmeddelande:\n\n"+e);
   }
   
-  
+  //Skeptisk kring att ha 'const' här, föreläsningen sa const > let > var,
+  //och det verkar fungera, dvs. tycks ändå kunna förändra variablerna,
+  //så jag låter det stå; kommentera gärna.
   const inputBlugs = document.getElementById("inputBlugs");
+  const blugDisplayWindow = document.getElementById("blugDisplayWindow");
   
   inputBlugs.oninput = function(e) {
     
@@ -46,12 +60,46 @@ Vad har jag gjort för att felsöka/undvika buggar?
   
     if(typeof(inputValue) == 'number') {
 
-      if(inputValue < 0) {
-        //set value to 0.
+      if(inputValue < 0) { //input cannot have value < 0.
         inputBlugs.value = 0;
       }
+      setNumberOfBlugs(inputValue);
+
+
     } else {
-      throw "the inputValue was not parsed into a number-value.";
+      throw "Unable to parse inputValue into a number-value.";
     }
+  }
+
+  function setNumberOfBlugs(count) {
+    //kolla hur många som finns
+    //lägg till/ta bort efter behov
+
+    //men först, bara skriv ut ett gäng.
+    for (let index = 0; index < count; index++) {
+      let createdBlug = createBlug(index);
+
+      blugDisplayWindow.appendChild(createdBlug);
+    }
+  }
+
+  function createBlug(index) {
+    let blugCard = document.createElement("div");
+    blugCard.setAttribute('class', 'blugCard');
+    blugCard.setAttribute('id', `blugCard${index}`);
+
+
+    //create title of Blug-card.
+    let title = document.createElement('div');
+    title.setAttribute('class', 'blugCardTitle');
+    title.innerText = 'Title';
+
+    let content = document.createElement('div');
+    content.setAttribute('class', 'blugCardContent');
+    content.innerText = loremIpsum;
+
+    blugCard.appendChild(content);
+
+    return blugCard;
   }
 })();
