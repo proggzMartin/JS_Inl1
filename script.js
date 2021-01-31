69b3452daf3fbc877a999b1da328541db452bcd4
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-01-31 10:11:12 +0100

    Added comments as assignment requires. Wrapped code in function(). added 'use strict'. added example of declariang variable without var, let or const, throwing error and catching it.

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+/* 
+Vad har jag gjort för att felsöka/undvika buggar?
+
+* 
+
+* debugger  https://www.w3schools.com/js/js_debugging.asp
+-I Chrome : F12 --> sources --> script.js --> markerar rad för debugging 
+--> använder F5, F9, F10, F11 för att steppa igenom kod.
+
+-console.log 
+- track:a ex. att input är string; om så konvertera till number
+- analysera förväntade värden.
+* ------------------
+
+* use strict https://www.w3schools.com/js/js_strict.asp
+- missar man deklarera variabel kastas ett error/exception.
+* ------------------
 
 
+*/
 
+//Knep för att undvika att variabler utanför blir global;
+//koden som definieras i filen hålls här. Se översta svaret:
+//https://stackoverflow.com/questions/1841916/how-to-avoid-global-variables-in-javascript
+
+(function() { 
+  "use strict";
+
+  try {
+    x1 = 3.14; //kastar fel p.g.a. strict mode.
+  } catch(e) {
+    console.log("Ett fel uppstod pga. strict mot inte tillåter "+
+                "icke-initialiserad variabler. Felmeddelande:\n\n"+e);
+  }
+  
+  
+  const inputBlugs = document.getElementById("inputBlugs");
+  
+  inputBlugs.oninput = function(e) {
+    
+    let inputValue = e.target.value;
+  
+    if(typeof(inputValue) == 'string'){
+      console.log("parsing from string to int");
+      inputValue = parseInt(inputValue);
+    }
+  
+    console.log(typeof(inputValue))
+  }
+})();
